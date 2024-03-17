b1b90dd4766b4765c2aef380ff843603dc055cb7
Evgene Vladimirovich Karkhanov <kx.eugene@gmail.com>
2024-03-17 23:12:22 +0300

    finish header layout

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -2,17 +2,25 @@ import eatlyLogo from "./assets/logo.svg";
 
 export default function Menu() {
   return (
-    <header className="border-b w-[1248px]">
-      <div>
-        <img src={eatlyLogo} alt="" />
-        eatly
-      </div>
+    <header className="border-b w-[1248px] flex items-center gap-12 pb-[34px] pt-[59px]">
+      <a href="/">
+        <div className="flex gap-2 items-center">
+          <img src={eatlyLogo} alt="" className="w-12" />
+          <span className="font-semibold text-[#6C5FBC] text-[22px]">
+            eatly
+          </span>
+        </div>
+      </a>
 
       <nav>
-        <ul>
+        <ul className="flex gap-12 text-[#606060] text-lg">
           <li>
             <a href="/recipes">Recipes</a>
+          </li>
+          <li>
             <a href="/faq">FAQ</a>
+          </li>
+          <li>
             <a href="/blog">Blog</a>
           </li>
         </ul>
